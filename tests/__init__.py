92544dcde6932b6d5debbfd25edb57a20f5e38ed
mikael10j <mikael.jacquemont@free.fr>
2026-06-07 17:56:08 +0200

    add Apache license

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,20 @@
 """
+Copyright 2026 mikael10j
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+"""
 Package de tests pour le projet livre de comptes.
 
 Ce package contient tous les tests du projet :
@@ -8,4 +24,4 @@ Ce package contient tous les tests du projet :
 """
 
 __version__ = "1.0.0"
-__author__ = "Livre de comptes - Les Vétérans du Basket"
+__author__ = "mikael10j"
